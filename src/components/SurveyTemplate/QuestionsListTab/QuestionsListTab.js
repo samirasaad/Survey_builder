066103722ff900dae2bfb55527c19628287dfdda
Samira <samirasaad@gmail.com>
2022-11-02 20:15:01 +0200

    handle list/grid view

--- src/components/SurveyTemplate/QuestionsListTab/QuestionsListTab.js
+++ src/components/SurveyTemplate/QuestionsListTab/QuestionsListTab.js
@@ -1,13 +1,24 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Modal from "../../../sharedUi/Modal/Modal";
-import SharedTooltip from "../../../sharedUi/Tooltip/Tooltip";
 import SingleQuestion from "./SingleQuestion";
-import Button from "@mui/material/Button";
 import Btn from "./../../../controls/Btn/Btn";
 
 const QuestionsListTab = ({ questionsList }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [viewType, setViewType] = useState("List"); // List || Grid
   const [questionID, setQuestionID] = useState(null);
+
+  useEffect(() => {
+    if (viewType === "Grid") {
+      Array.from(document.querySelectorAll(".single-question")).forEach(
+        (elem) => elem.classList.add("col-lg-4", "col-md-6")
+      );
+    } else if (viewType === "List") {
+      Array.from(document.querySelectorAll(".single-question")).forEach(
+        (elem) => elem.classList.remove("col-lg-4", "col-md-6")
+      );
+    }
+  }, [viewType]);
 
   const handleModalState = (e, modalState, questionID) => {
     setQuestionID(questionID);
@@ -31,33 +42,27 @@ const QuestionsListTab = ({ questionsList }) => {
     </>
   );
 
+  const handleViewType = (e, type) => {
+    setViewType(type);
+  };
+
   return questionsList ? (
     questionsList.length > 0 ? (
       <>
-        {questionsList.map((question) => (
-          <div className="d-flex">
-            <SingleQuestion question={question} key={question.id} />
-            <SharedTooltip
-              tooltipTargetElement={
-                <Button onClick={(e) => handleModalState(e, true, question.id)}>
-                  Arrow
-                </Button>
-              }
-              title="Edit question"
+        <div className="d-flex">
+          <Btn content="Grid" handleClick={(e) => handleViewType(e, "Grid")} />
+          <Btn content="List" handleClick={(e) => handleViewType(e, "List")} />
+        </div>
+        <div className="row">
+          {questionsList.map((question) => (
+            <SingleQuestion
+              className="single-question col-12"
+              question={question}
+              key={question.id}
+              handleModalState={handleModalState}
             />
-
-            <Btn
-              className="mx-2"
-              handleClick={(e) => handleModalState(e, true, question.id)}
-              content="duplicate"
-            />
-            <Btn
-              className="mx-2"
-              handleClick={(e) => handleModalState(e, true, question.id)}
-              content="delete"
-            />
-          </div>
-        ))}
+          ))}
+        </div>
         <Modal
           isOpen={isOpen}
           renderModalContent={(e) => renderModalContent(e)}
--- src/components/SurveyTemplate/QuestionsListTab/SingleQuestion.js
+++ src/components/SurveyTemplate/QuestionsListTab/SingleQuestion.js
@@ -1,4 +1,8 @@
-const SingleQuestion = ({ question }) => {
+import SharedTooltip from "../../../sharedUi/Tooltip/Tooltip";
+import Button from "@mui/material/Button";
+import Btn from "../../../controls/Btn/Btn";
+
+const SingleQuestion = ({ question, handleModalState,className }) => {
   const renderQuestionType = (type) => {
     switch (type) {
       case 1:
@@ -13,9 +17,28 @@ const SingleQuestion = ({ question }) => {
   };
 
   return (
-    <div className="d-flex">
+    <div className={`d-flex ${className}`}>
       <p>{question.content}</p>
       <p>{renderQuestionType(question.type)}</p>
+      <SharedTooltip
+        tooltipTargetElement={
+          <Button onClick={(e) => handleModalState(e, true, question.id)}>
+            Arrow
+          </Button>
+        }
+        title="Edit question"
+      />
+
+      <Btn
+        className="mx-2"
+        handleClick={(e) => handleModalState(e, true, question.id)}
+        content="duplicate"
+      />
+      <Btn
+        className="mx-2"
+        handleClick={(e) => handleModalState(e, true, question.id)}
+        content="delete"
+      />
     </div>
   );
 };
