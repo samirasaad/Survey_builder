955c77330c953c0c9169f625d391b63cceadfccc
Samira <samirasaad@gmail.com>
2022-11-28 15:31:02 +0200

    adding template hedaer section displays template title

--- src/components/TemplateHeader/TemplateHeader.js
+++ src/components/TemplateHeader/TemplateHeader.js
@@ -1,22 +1,27 @@
-import { doc, getDoc, onSnapshot } from "firebase/firestore";
+import { doc, onSnapshot } from "firebase/firestore";
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import { DB } from "../../firebase";
 import { TEMPLATES } from "../../utils/constants";
 
 const TemplateHeader = () => {
-  const [templateData, setTemplateData] = useState("");
+  const templateId =
+    useParams().templateId || localStorage.getItem("templateId");
+  const [templateData, setTemplateData] = useState(null);
+
   useEffect(() => {
     let mounted = true;
-    if (mounted) {
+    if (mounted && templateId) {
       getTemplateData();
     }
     return () => {
       mounted = false;
     };
-  }, []);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [templateId]);
 
-  const getTemplateData = async () => {
-    /**************** GETTING TEMPLATE DATA ONCE ON INTIALIZING COMPONENT *************/
+  const getTemplateData = () => {
+    /****************** GETTING TEMPLATE DATA ONCE ON INTIALIZING COMPONENT *******************/
     // const docRef = doc(DB, TEMPLATES, localStorage.getItem("templateId"));
     // const docSnap = await getDoc(docRef);
     // if (docSnap.exists()) {
@@ -27,20 +32,20 @@ const TemplateHeader = () => {
     //   console.log("No such document!");
     // }
 
-    /**************** SUBSCRIBING ON ANY CHANGE AS LONG AS IN THIS SCREEN ****************/
-    onSnapshot(
-      doc(DB, TEMPLATES, localStorage.getItem("templateId")),
-      (doc) => {
+    /**************** SUBSCRIBING ON ANY CHANGE AS LONG AS  USER IN THIS SCREEN ****************/
+    if (templateId) {
+      onSnapshot(doc(DB, TEMPLATES, templateId), (doc) => {
         if (doc.exists()) {
           console.log("Current data: ", doc.data());
           setTemplateData({ ...doc.data() });
         } else {
           console.log("No such document!");
         }
-      }
-    );
+      });
+    }
   };
-  return templateData?.title && <p>{templateData?.title}</p>;
+
+  return templateData?.title ? <p>{templateData?.title}</p> : <></>;
 };
 
 export default TemplateHeader;
